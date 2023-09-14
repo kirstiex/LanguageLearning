fd055c7acca38f30e1cbb378771cc45bfc9e442b
Vincent <106598307+AVincent-Zag@users.noreply.github.com>
2023-09-14 13:05:17 -0600

    workign cards and added delete and pronunciation button but needs routes

--- Platform/mongo_app.py
+++ Platform/mongo_app.py
@@ -152,26 +152,21 @@ def create_card():
 #Delete Question
 @app.route('/card/delete', methods=['POST'])
 def delete_card():
-    if not is_user_logged_in():
-        return jsonify({"message": "You must be logged in to delete a Card."}), 401
-    user = users_collection.find_one({'_id': ObjectId(session['user_id'])})
+    user_logged_in = is_user_logged_in()
+    first_name = None
+    if user_logged_in:
+        user = users_collection.find_one({'_id': ObjectId(session['user_id'])})
+        if user:
+            first_name = user.get('first_name')
 
-    if not user:
-        return jsonify({"message": "You must be logged in to delete a Card."}), 401
-    data = request.get_json()
+    search_term = request.args.get('searchTerm')
+    category = request.args.get('category')
     
-    if not data:
-        return jsonify({"message": "Request data missing."}), 400
-    english = data.get('english')
-    if not english:
-        return jsonify({"message": "Please provide the English translation of the card to delete."}), 400
-
-
-    deleted_card = cards_collection.find_one_and_delete({'english': english})
-    if not deleted_card:
-        return jsonify({"message": "Card not found."}), 404
-
-    return jsonify({"message": "Card deleted successfully."}), 200
+    # Build the MongoDB query based on search_term and category (if provided)
+    query = {}
+    cards = cards_collection.delete_one(query)
+    
+    return render_template("cards.html", cards= cards, user_logged_in=user_logged_in, first_name=first_name)
 
 @app.route('/card/list', methods=['GET'])
 def get_card_list():
